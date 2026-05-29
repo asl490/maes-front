bd98e742f48ad4757339ed77ffc22c55a06050cb
asl490 <asl490.np@gmail.com>
2026-05-29 18:35:40 -0500

    Refactor code structure for improved readability and maintainability

--- src/app/pages/landing/components/cta/cta.component.ts
+++ src/app/pages/landing/components/cta/cta.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cta',
   standalone: true,
-  templateUrl: './cta.component.html'
+  imports: [RouterLink],
+  templateUrl: './cta.component.html',
 })
 export class CtaComponent {}
